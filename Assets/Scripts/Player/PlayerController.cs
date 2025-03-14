308f4528b65537d22d17bb8a4b625f7f55c1360e
Nestor Batoon <batoonnestor@gmail.com>
2025-03-14 22:43:07 +1100

    Player movement work

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,24 +3,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("General movement Settings")]
     [SerializeField] private float jogSpeed = 5f;
     [SerializeField] private float runSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float groundCheckDistance = 1f;
     [SerializeField] private ViewState currentViewState = ViewState.SideScrolling;
     
+    [SerializeField] private float linearDrag = 6.5f;
+    
     private Rigidbody2D rb2d;
     private CapsuleCollider2D capsule2d;
     private PlayerInput _playerInput;
-    private InputAction _moveAction, _jumpAction, _sprintAction, _crouchAction;
+    private InputAction _moveAction, _jumpAction, _sprintAction;
     private SpriteRenderer spriteRenderer;
-    private float currentMoveSpeed = 0f;
+    private float currentMoveSpeed = 0f, startingDrag;
     private Vector2 currrentMovementVector;
     
 
     private static PlayerController instance;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake() 
+    {
+        if (instance == null)
+         {
+         instance = this;
+         DontDestroyOnLoad(gameObject);
+         }
+          else
+            {
+                Destroy(gameObject);
+            }
+        }
+        
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -28,13 +43,14 @@ public class PlayerController : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>(); 
         currentMoveSpeed = jogSpeed;
         InitalizeActions();
-         
+        startingDrag = rb2d.linearDamping;
     }
 
     
     void Update()
     {
         ManageMovement();
+        ManageSprite();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -56,15 +72,10 @@ public class PlayerController : MonoBehaviour
         _sprintAction = _playerInput.actions.FindAction("Sprint");
         _sprintAction.Enable();
         _sprintAction.performed += SprintAction;  
-        _sprintAction.canceled += ctx => currentMoveSpeed = jogSpeed;
-        
-        _crouchAction = _playerInput.actions.FindAction("Crouch");
-        _crouchAction.Enable();
-        _crouchAction.performed += CrouchAction;
+        _sprintAction.canceled += ctx => currentMoveSpeed = jogSpeed; 
     }
     private void SprintAction(InputAction.CallbackContext context) { currentMoveSpeed = runSpeed; }
-    private void JumpAction(InputAction.CallbackContext context) { if(IsGrounded())rb2d.AddForce(transform.up * jumpForce, ForceMode2D.Impulse); }
-    private void CrouchAction(InputAction.CallbackContext context) { /* TODO: Implement proper logic for crouching */ }
+    private void JumpAction(InputAction.CallbackContext context) { if(IsGrounded())rb2d.AddForce(transform.up * jumpForce, ForceMode2D.Impulse); } 
     #endregion
     
     #region Helper Functions
@@ -75,22 +86,47 @@ public class PlayerController : MonoBehaviour
         {
             currrentMovementVector = _moveAction.ReadValue<Vector2>();  
             if (currentViewState == ViewState.SideScrolling)
-            {
-                rb2d.AddForce(new Vector2(currrentMovementVector.x * currentMoveSpeed, 0));
+            { 
                 rb2d.gravityScale = 1; 
                 _jumpAction.Enable();
+                rb2d.linearDamping = startingDrag;
+                /*
+                 * Crouching
+                 * if currrentMovementVector is less then 0, then that means the player is inputing down or crouch
+                 * when the player is crouching, they are not allowed to move
+                 */
+                if(currrentMovementVector.y >= 0)
+                    rb2d.AddForce(new Vector2(currrentMovementVector.x * currentMoveSpeed, 0));
             }
             else
-            {
-                rb2d.AddForce(currrentMovementVector * currentMoveSpeed * 0.75f);
+            { 
                 rb2d.gravityScale = 0; 
-                _jumpAction.Disable(); 
+                _jumpAction.Disable();  
+                rb2d.linearDamping = linearDrag; 
+                rb2d.AddForce(currrentMovementVector * currentMoveSpeed * 0.75f); 
             }
         } 
+        
         private bool IsGrounded() {return Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, ~LayerMask.GetMask("Player")); }
         
         #endregion
-        
+
+        #region Sprite-Related Functions
+
+        private void ManageSprite()
+        {
+            /*PLACE HOLDER*/
+            /* TODO: Implement proper logic/ aniamtion change for when the player is crocuhing */
+            if(currentViewState == ViewState.SideScrolling)
+            {
+                if (currrentMovementVector.y < 0)
+                    transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                else
+                    transform.localScale = Vector3.one;
+            }
+        }
+
+        #endregion
     #endregion
     
     #region Public Methods
@@ -102,18 +138,5 @@ public class PlayerController : MonoBehaviour
 public enum ViewState
 {
     SideScrolling,
-    TopDown
-
-    private void Awake() 
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+    TopDown 
 }
