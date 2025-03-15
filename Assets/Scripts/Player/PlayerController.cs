f922a798562628a6d406448a693307c1babaa076
Nestor Batoon <batoonnestor@gmail.com>
2025-03-16 00:17:16 +1100

    animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -19,9 +19,6 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private float currentMoveSpeed = 0f, startingDrag;
     private Vector2 currrentMovementVector;
-    
-
-    private static PlayerController instance; 
         
     void Start()
     {
@@ -38,11 +35,6 @@ public class PlayerController : MonoBehaviour
     {
         ManageMovement();
         ManageSprite();
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        
     }
     
     #region Action Defintions
