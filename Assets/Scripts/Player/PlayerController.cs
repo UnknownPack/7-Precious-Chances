a81c135e00814a08aa780cea6fb3328220006b25
MathgeniusTB2 <daniel.j.martirosov@student.uts.edu.au>
2025-03-16 18:07:08 +1100

    added sound effects to Player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -71,8 +71,6 @@ public class PlayerController : MonoBehaviour
             _audioSource.clip = jumpingSound;
             _audioSource.loop = false;
             _audioSource.Play();
-            Debug.Log("Jumping sound: " + jumpingSound);
-
         }
     } 
     #endregion
@@ -94,23 +92,37 @@ public class PlayerController : MonoBehaviour
                  * if currrentMovementVector is less then 0, then that means the player is inputing down or crouch
                  * when the player is crouching, they are not allowed to move
                  */
-                if(currrentMovementVector.y >= 0) {
+                if(!IsMovingVertically()) {
                     rb2d.AddForce(new Vector2(currrentMovementVector.x * currentMoveSpeed, 0));
                     if (currrentMovementVector.x != 0) {
-                    if (IsGrounded()) {
-                        if (_audioSource.clip != walkingSound || !_audioSource.isPlaying) {
-                            _audioSource.clip = walkingSound;
-                            _audioSource.loop = true;
-                            _audioSource.Play();
+                        if (IsGrounded()) {
+                            if (_audioSource.clip != walkingSound || !_audioSource.isPlaying) {
+                                _audioSource.clip = walkingSound;
+                                _audioSource.loop = true;
+                                _audioSource.Play();
+                            }
                         }
-                    }
-                    } else {
-                        if (_audioSource.isPlaying && _audioSource == walkingSound) {
+                        else {
+                            if (_audioSource.isPlaying && _audioSource.clip == walkingSound) {
+                            _audioSource.Stop();
+                            _audioSource.loop = false;
+                        }
+                        }
+                    } 
+                    else {
+                        if (_audioSource.isPlaying && _audioSource.clip == walkingSound) {
                             _audioSource.Stop();
                             _audioSource.loop = false;
                         }
                     }
                 }
+                else {
+                    if (_audioSource.isPlaying && _audioSource.clip == walkingSound) {
+                            _audioSource.Stop();
+                            _audioSource.loop = false;
+                        }
+                }
+                
                 
 
                     
@@ -120,11 +132,25 @@ public class PlayerController : MonoBehaviour
                 rb2d.gravityScale = 0; 
                 _jumpAction.Disable();  
                 rb2d.linearDamping = linearDrag; 
-                rb2d.AddForce(currrentMovementVector * currentMoveSpeed * 0.75f);     
+                rb2d.AddForce(currrentMovementVector * currentMoveSpeed * 0.75f);
+                if (currrentMovementVector.magnitude != 0) {
+                        if (_audioSource.clip != walkingSound || !_audioSource.isPlaying) {
+                            _audioSource.clip = walkingSound;
+                            _audioSource.loop = true;
+                            _audioSource.Play();
+                        }
+                    } 
+                    else {
+                        if (_audioSource.isPlaying && _audioSource.clip == walkingSound) {
+                            _audioSource.Stop();
+                            _audioSource.loop = false;
+                        }
+                    }    
             }
         } 
         
         private bool IsGrounded() {return Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, ~LayerMask.GetMask("Player", "Ignore Raycast")); }
+        private bool IsMovingVertically() {return Mathf.Abs(rb2d.linearVelocityY) > 0.1f;}
         
         #endregion
 
@@ -132,12 +158,23 @@ public class PlayerController : MonoBehaviour
 
         private void ManageSprite()
         {
-            if(currrentMovementVector.x != 0)
+            if(currrentMovementVector.x != 0 && IsGrounded())
             {
                 _animator.Play("Walk"); 
                 _animator.speed = Mathf.Approximately(currentMoveSpeed, jogSpeed)? 1f : 2.5f;
                 _capsule2d.offset = Vector2.zero;
                 _capsule2d.size = new Vector2(2f, 1f);
+                if (_audioSource.clip != walkingSound && !_audioSource.isPlaying) {
+                    _audioSource.clip = walkingSound;
+                    _audioSource.loop = true;
+                    _audioSource.Play();
+                }
+            }
+            else {
+                if (_audioSource.isPlaying && _audioSource.clip == walkingSound) {
+                    _audioSource.Stop();
+                    _audioSource.loop = false;
+                }
             }
             
             if(currentViewState == ViewState.SideScrolling && currrentMovementVector.y < 0)
@@ -145,6 +182,10 @@ public class PlayerController : MonoBehaviour
                 _animator.Play("Crouch");
                 _capsule2d.offset = new Vector2(-0.2f, -0.15f);
                 _capsule2d.size = new Vector2(1.5f, 1.75f);
+                if (_audioSource.isPlaying && _audioSource.clip == walkingSound) {
+                    _audioSource.Stop();
+                    _audioSource.loop = false;
+                }
             }
             else if(currrentMovementVector.y == 0 && currrentMovementVector.x == 0 &&!_isDead)
             {
